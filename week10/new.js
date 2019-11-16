b734c8a849a8af32c3a70633564e47a2d324e262
Watermelon <1050364782@qq.com>
2019-11-16 16:18:59 +0800

    week10 1.1 des:multi selected problem solved

--- week10/new.js
+++ week10/new.js
@@ -26,26 +26,62 @@ $(function(){
         }
 
         var oriservers = $(".server-choose").next();
+        //当服务和服务器都被选中时才进行添加
         if( span!=null && oriservers.is("div")){
-            var first = oriservers.children("tr:first-child");
-            var last = oriservers.children("tr:last-child");
-            alert(first.innerHTML);
-            while(first != last){
-                console.log(first.children().innerText);
-                if(first.children().innerText==str){
-                    alert("此服务已经被添加！");
+            var first = oriservers.children("div");
+            // alert(first[0].innerHTML);
+            // alert(str)
+            // alert(first[0].innerHTML==str);
+            // alert(first.length);
+            // 判断服务是否被添加
+            var i = 0
+            while(i<first.length){
+                if(first[i].innerHTML==str){
+                    alert("此服务已经被添加");
                     return;
                 }
-                first = first.next();
+                i++;
             }
 
-            var newApp = '<div class="app"><span>'+str+'</span></div>';
+            var newApp = '<div class="app">'+str+'</div>';
             oriservers.append(newApp);
         }else{
             // var newSpan = document.createElement("span");
             // newSpan.innerText = str;
             alert("没有选择服务或服务器");
         }
+    });
+
+    $("#del").click(function(){
+        //删除每个oriserver中选中的服务
+        // var oriservers = $(".oriservers");
+        // var i = 0;
+        // while(i<oriservers.length){
+        //     alert(oriservers[i].innerHTML);
+        //     var toremove = oriservers.children(".app-choose");
+        //     alert(toremove.length);
+        //     alert(oriservers.length+","+i);
+        //     var j = 0;
+        //     while(j<toremove.length){
+        //         // alert(toremove[toremove.length-1].innerHTML);
+        //         oriservers[i].removeChild(toremove[j]);
+        //         j++;
+        //         alert("oriservers "+i+"\r\ntoremove "+j);
+        //     }
+        //     alert("oriservers"+i);
+        //     i++;
+        // }
+        var oriservers = $(".oriservers");
+        var toremove = oriservers.children(".app-choose");
+        var j = 0;
+        while(j<toremove.length){
+            // alert(toremove[toremove.length-1].innerHTML);
+            // oriservers[0].remo   veChild(toremove[j]);
+            oriservers[1].removeChild(toremove[j]);
+            j += 1;
+            alert(j);
+        }
+        
     });
 
 });
